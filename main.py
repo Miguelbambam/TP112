df146a5f967814028cc3ca221e24b3ca4bd96757
Michael <hjabiab@gmail.com>
2025-07-26 17:23:37 -0400

    added pawn class, pawns can move now, whimsical innit!

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
 from cmu_graphics import *
 
-# TODO: 
-# Make classes for each piece
+
+"""
+Citations:
+https://www.w3schools.com/python/python_inheritance.asp
+"""
 
 class Piece:
     def __init__(self, type, color):
         self.type = type
         self.color = color
 
+class Pawn(Piece):
+    def __init__(self, color):
+        super().__init__('pawn', color)
+
+    def getMoves(self, board, row, col):
+        moves = []
+        direction = -1 if self.color == 'white' else 1
+        startRow = 6 if self.color == 'white' else 1
+
+        if 0 <= row + direction < 8 and board[row + direction][col] is None:
+            moves.append((row + direction, col))
+
+            if row == startRow and board[row + 2 * direction][col] is None:
+                moves.append((row + 2 * direction, col))
+
+        for dc in [-1, 1]:
+            newCol = col + dc
+            newRow = row + direction
+            if 0 <= newCol < 8 and 0 <= newRow < 8:
+                target = board[newRow][newCol]
+                if target is not None and target.color != self.color:
+                    moves.append((newRow, newCol))
+
+        return moves
+
 class ChessGame:
     def __init__(self):
         self.board = [[None] * 8 for _ in range(8)]
         self.setupPieces()
+        self.selectedPiece = None
+        self.validMoves = []
+
     
     def setupPieces(self):
+        backRow = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
         for col in range(8):
-            self.board[1][col] = Piece('pawn', 'black')
-            self.board[6][col] = Piece('pawn', 'white')
+            self.board[1][col] = Pawn('black')
+            self.board[6][col] = Pawn('white')
+        
+        for piece in range(len(backRow)):
+            self.board[0][piece] = Piece(backRow[piece], 'black')
+            self.board[7][piece] = Piece(backRow[piece], 'white')
 
 def onAppStart(app):
     app.width = 600
@@ -25,6 +61,34 @@ def onAppStart(app):
     app.squareSize = app.boardSize // 8
     app.statusMessage = "White's turn"
     app.game = ChessGame()
+
+def onMousePress(app, x, y):
+    row = y // app.squareSize
+    col = x // app.squareSize
+
+    if row >= 8 or col >= 8: return
+
+    board = app.game.board
+    selected = app.game.selectedPiece
+    validMoves = app.game.validMoves
+
+    if selected and (row, col) in validMoves:
+        ogRow, ogCol = selected
+        board[row][col] = board[ogRow][ogCol]
+        board[ogRow][ogCol] = None
+        app.game.selectedPiece = None
+        app.game.validMoves = []
+        app.statusMessage = "Black's turn" if app.statusMessage.startswith("White") else "White's turn"
+        return
+
+    piece = board[row][col]
+    if piece and piece.type == 'pawn':
+        moves = piece.getMoves(board, row, col)
+        app.game.selectedPiece = (row, col)
+        app.game.validMoves = moves
+    else:
+        app.game.selectedPiece = None
+        app.game.validMoves = []
 
 def drawPieces(app):
     for r in range(8):
